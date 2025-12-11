e228fadb4eb11997580a47dd89880426ded7d8f4
Gareth <gareth@redovoting.io>
2025-12-11 11:49:16 -0500

    Spring boot and health controller implemented

---
+++ ValensMockProject/src/main/java/org/example/valensmockproject/ValensMockProjectApplication.java
@@ -0,0 +1,12 @@
+package org.example.valensmockproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValensMockProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValensMockProjectApplication.class, args);
+    }
+}
